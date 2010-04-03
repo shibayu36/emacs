5eda73387c57b0226cbfc2b41cfc52c6adaee74a
Yuki Shibazaki <shiba1029196473@gmail.com>
2010-04-04 00:50:14 +0900

    python-modeでtabをスペースに

--- emacs.d/init-edit-mode.el
+++ emacs.d/init-edit-mode.el
@@ -44,9 +44,9 @@
 (autoload 'python-mode "python-mode" "Major mode for editing Python programs" t)
 (autoload 'py-shell "python-mode" "Python shell" t)
 (setq auto-mode-alist (cons '("\\.py\\'" . python-mode) auto-mode-alist))
-;; (add-hook 'python-mode-hook
-;;           (function (lambda ()
-;;                       (setq indent-tabs-mode nil))))
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq indent-tabs-mode nil))))
 (add-hook 'python-mode-hook '(lambda ()
                                (require 'pycomplete)
                                ))

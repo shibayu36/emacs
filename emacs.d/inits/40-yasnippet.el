12bc5447b31a176af2c1e210b062087bc857d216
shiba_yu36 <shibayu36@gmail.com>
2011-02-28 13:49:20 +0900

    yasnippetを調整

--- emacs.d/inits/40-yasnippet.el
+++ emacs.d/inits/40-yasnippet.el
@@ -4,4 +4,4 @@
 (setq anything-c-yas-space-match-any-greedy t) ;スペース区切りで絞り込めるようにする デフォルトは nil
 (global-set-key (kbd "C-c y") 'anything-c-yas-complete) ;C-c yで起動 (同時にお使いのマイナーモードとキーバインドがかぶるかもしれません)
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/elisp/yasnippet/snippets/") ;snippetsのディレクトリを指定
+(yas/load-directory "~/.emacs.d/snippets/") ;snippetsのディレクトリを指定

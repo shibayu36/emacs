a258967252e05c5f4f2788705e566d1c4c7f9a14
shiba_yu36 <shibayu36@gmail.com>
2011-03-08 01:14:10 +0900

    compileのsplit-root対応

--- emacs.d/inits/40-mode-compile.el
+++ emacs.d/inits/40-mode-compile.el
@@ -5,3 +5,13 @@
 (setq mode-compile-always-save-buffer-p t)
 (setq mode-compile-never-edit-command-p t)
 (setq mode-compile-reading-time 0)
+
+(setq compilation-window-height 18)
+(when (>= emacs-major-version 23)
+
+  (defun my-split-window-by-split-root (window)
+    (if (or (compilation-buffer-p buffer)
+            (equal name-of-buffer "*Shell Command Output*"))
+        (split-root-window compilation-window-height)
+      (split-window-sensibly window)))
+  (setq split-window-preferred-function 'my-split-window-by-split-root))

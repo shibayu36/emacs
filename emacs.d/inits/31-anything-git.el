97c03e5495a84acf2620fd917089702b085b2f3b
shiba_yu36 <shibayu36@gmail.com>
2011-03-17 13:43:50 +0900

    snippets, anything-git

--- emacs.d/inits/31-anything-git.el
+++ emacs.d/inits/31-anything-git.el
@@ -6,5 +6,5 @@
   (defun anything-git-grep-all ()
     "git grep project and submodule."
     (interactive)
-    (anything anything-git-grep-all-sources (thing-at-point 'symbol) nil nil nil "*anything git grep all*"))
+    (anything anything-git-grep-all-sources nil nil nil nil "*anything git grep all*"))
   )

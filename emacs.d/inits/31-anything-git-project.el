f1330a8610ebb9139387bbedce7f5bbaf91796ae
shiba_yu36 <shibayu36@gmail.com>
2012-12-22 19:02:08 +0900

    anything-git-projectの調整

--- emacs.d/inits/31-anything-git-project.el
+++ emacs.d/inits/31-anything-git-project.el
@@ -31,39 +31,44 @@
 (when (require 'anything nil t)
 
   (defun anything-git-project-is-git-repository ()
-    (interactive)
-    (let ((is-git-repository (shell-command "git rev-parse")))
-      (if )
-      (message "%d" is-git-repository)
+    (let ((error-message (shell-command-to-string "git rev-parse")))
+      (if (string= error-message "")
+          t
+        nil)
       ))
 
   (defun anything-git-project-project-dir ()
     (chomp
-     (shell-command-to-string "git rev-parse --show-toplevel")))
+     (shell-command-to-string "git rev-parse --show-toplevel")
+     ))
 
   (defun anything-c-sources-git-project-for ()
-    (loop for elt in
-          '(("Modified files (%s)" . "--modified")
-            ("Untracked files (%s)" . "--others --exclude-standard")
-            ("All controlled files in this project (%s)" . ""))
-          collect
-          `((name . ,(format (car elt) (anything-git-project-project-dir)))
-            (init . (lambda ()
-                      (setq current-git-project-dir
-                            (anything-git-project-project-dir))
-                      (unless (and ,(string= (cdr elt) "") ;update candidate buffer every time except for that of all project files
-                                   (anything-candidate-buffer))
-                        (with-current-buffer
-                            (anything-candidate-buffer 'global)
-                          (insert
-                           (shell-command-to-string
-                            ,(format "git ls-files --full-name $(git rev-parse --show-cdup) %s"
-                                     (cdr elt))))))))
-            (candidates-in-buffer)
-            (display-to-real . (lambda (name)
-                                 (format "%s/%s"
-                                         current-git-project-dir name)))
-            (type . file))))
+    (cond ((anything-git-project-is-git-repository)
+           (loop for elt in
+                 '(("Modified files (%s)" . "--modified")
+                   ("Untracked files (%s)" . "--others --exclude-standard")
+                   ("All controlled files in this project (%s)" . ""))
+                 collect
+                 `((name . ,(format (car elt) (anything-git-project-project-dir)))
+                   (init . (lambda ()
+                             (setq current-git-project-dir
+                                   (anything-git-project-project-dir))
+                             (unless (and ,(string= (cdr elt) "") ;update candidate buffer every time except for that of all project files
+                                          (anything-candidate-buffer))
+                               (with-current-buffer
+                                   (anything-candidate-buffer 'global)
+                                 (insert
+                                  (shell-command-to-string
+                                   ,(format "git ls-files --full-name $(git rev-parse --show-cdup) %s"
+                                            (cdr elt))))))))
+                   (candidates-in-buffer)
+                   (display-to-real . (lambda (name)
+                                        (format "%s/%s"
+                                                current-git-project-dir name)))
+                   (type . file))
+           ))
+          ((list))
+          ))
 
   (defun anything-git-project ()
     (interactive)

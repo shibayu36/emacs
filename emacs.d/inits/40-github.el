ded1b37c3183d22ec97a9ca551124a5fe3cfdda2
shiba_yu36 <shibayu36@gmail.com>
2013-01-17 10:48:07 +0900

    githubひらくやつ改良

--- emacs.d/inits/40-github.el
+++ emacs.d/inits/40-github.el
@@ -1,6 +1,12 @@
 (defun open-github-from-current ()
   (interactive)
-  (cond ((git-project-p)
+  (cond ((and (git-project-p) (use-region-p))
+         (shell-command
+          (format "open-github-from-file %s %d %d"
+                  (file-name-nondirectory (buffer-file-name))
+                  (line-number-at-pos (region-beginning))
+                  (line-number-at-pos (region-end)))))
+        ((git-project-p)
          (shell-command
           (format "open-github-from-file %s %d"
                   (file-name-nondirectory (buffer-file-name))

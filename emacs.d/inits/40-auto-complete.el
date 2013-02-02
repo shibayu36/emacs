a2408ea49eb0db4567a226172c50407b767d2b2b
shiba_yu36 <shibayu36@gmail.com>
2013-02-02 14:28:02 +0900

    ac

--- emacs.d/inits/40-auto-complete.el
+++ emacs.d/inits/40-auto-complete.el
@@ -1,5 +1,7 @@
 ;;auto-complete-modeの読み込み
 (require 'auto-complete-config)
+(setq ac-delay 0.01)
+(setq ac-auto-show-menu 0.2)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
 (ac-config-default)
 (setq ac-use-menu-map t)

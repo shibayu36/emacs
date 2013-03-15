9ddf8059605afe42bc096bcb6be32461192acd10
shiba_yu36 <shibayu36@gmail.com>
2013-03-15 09:57:31 +0900

    rcodetools

--- emacs.d/inits/20-edit-mode-ruby.el
+++ emacs.d/inits/20-edit-mode-ruby.el
@@ -9,10 +9,6 @@
       (append '(("Capfile$" . ruby-mode)) auto-mode-alist))
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
                                      interpreter-mode-alist))
-;; (autoload 'run-ruby "inf-ruby"
-;;   "Run an inferior Ruby process")
-;; (autoload 'inf-ruby-keys "inf-ruby"
-;;   "Set local key defs for inf-ruby in ruby-mode")
 
 (add-hook 'ruby-mode-hook
           '(lambda ()
@@ -21,44 +17,15 @@
              (setq ruby-deep-indent-paren-style nil)
              (define-key ruby-mode-map [return] 'ruby-reindent-then-newline-and-indent)))
 
-(require 'align)
-(add-to-list 'align-rules-list
-             '(ruby-comma-delimiter
-               (regexp . ",\\(\\s-*\\)[^# \t\n]")
-               (repeat . t)
-               (modes  . '(ruby-mode))))
-(add-to-list 'align-rules-list
-             '(ruby-hash-literal
-               (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
-               (repeat . t)
-               (modes  . '(ruby-mode))))
-(add-to-list 'align-rules-list
-             '(ruby-assignment-literal
-               (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
-               (repeat . t)
-               (modes  . '(ruby-mode))))
-(add-to-list 'align-rules-list          ;TODO add to rcodetools.el
-             '(ruby-xmpfilter-mark
-               (regexp . "\\(\\s-*\\)# => [^#\t\n]")
-               (repeat . nil)
-               (modes  . '(ruby-mode))))
-
-
-;; rails
-;; (defun try-complete-abbrev (old)
-;;   (if (expand-abbrev) t nil))
-;; (setq hippie-expand-try-functions-list
-;;       '(try-complete-abbrev
-;;         try-complete-file-name
-;;         try-expand-dabbrev))
-;; (setq rails-use-mongrel t)
-;; (require 'rails)
-
 ;; ruby-block
 (require 'ruby-block)
 (ruby-block-mode t)
 (setq ruby-block-highlight-toggle t)
 
+;; ruby-electric
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
 (setq ruby-electric-expand-delimiters-list nil)
+
+(require 'rcodetools)
+(define-key ruby-mode-map (kbd "C-c C-d") 'xmp)

8dcbf4293b03c84b22d534c0ffbbc63f3ae6e280
shiba_yu36 <shibayu36@gmail.com>
2013-03-21 07:37:56 +0900

    rsense

--- emacs.d/inits/20-edit-mode-ruby.el
+++ emacs.d/inits/20-edit-mode-ruby.el
@@ -24,3 +24,14 @@
 
 (require 'rcodetools)
 (define-key ruby-mode-map (kbd "C-c C-d") 'xmp)
+
+;; rsense
+(setq rsense-home "/usr/local/Cellar/rsense/0.3/libexec")
+(require 'rsense)
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             ;; .や::を入力直後から補完開始
+             (add-to-list 'ac-sources 'ac-source-rsense-method)
+             (add-to-list 'ac-sources 'ac-source-rsense-constant)
+             ;; C-x .で補完出来るようキーを設定
+             (define-key ruby-mode-map (kbd "C-x .") 'ac-complete-rsense)))

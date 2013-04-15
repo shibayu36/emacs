bc58077d84302bc6d5411c2106b0c57282cf8375
shiba_yu36 <shibayu36@gmail.com>
2013-04-15 23:58:12 +0900

    open-github-from-current要らなくなった

--- emacs.d/inits/40-github.el
+++ emacs.d/inits/40-github.el
@@ -1,15 +1,1 @@
 (require 'open-github-from-here)
-
-(defun open-github-from-current ()
-  (interactive)
-  (cond ((and (git-project-p) (use-region-p))
-         (shell-command
-          (format "open-github-from-file %s %d %d"
-                  (file-name-nondirectory (buffer-file-name))
-                  (line-number-at-pos (region-beginning))
-                  (line-number-at-pos (region-end)))))
-        ((git-project-p)
-         (shell-command
-          (format "open-github-from-file %s %d"
-                  (file-name-nondirectory (buffer-file-name))
-                  (line-number-at-pos))))))

5590727779b86d783894d0f3cd94739e8eaf0d70
shiba_yu36 <shibayu36@gmail.com>
2013-04-27 19:40:40 +0900

    melpa

---
+++ emacs.d/elisp/elpa/melpa-0.3/melpa-pkg.el
@@ -0,0 +1,1 @@
+(define-package "melpa" "0.3" "special handling for the MELPA repository" (quote nil))
---
+++ emacs.d/inits/10-package.el
@@ -0,0 +1,4 @@
+(require 'package)
+(setq package-user-dir "~/.emacs.d/elisp/elpa/")
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)

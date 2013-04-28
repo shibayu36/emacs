802623cf8ef3a5e7201e1edd7fe39d21e29e37df
shiba_yu36 <shibayu36@gmail.com>
2013-04-28 16:15:20 +0900

    package

--- emacs.d/inits/10-package.el
+++ emacs.d/inits/10-package.el
@@ -1,4 +1,6 @@
 (require 'package)
+
 (setq package-user-dir "~/.emacs.d/elisp/elpa/")
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)

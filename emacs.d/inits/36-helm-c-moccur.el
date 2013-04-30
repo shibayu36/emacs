01f753ac7b2279decda949f98bd84a4aa498cbea
shiba_yu36 <shibayu36@gmail.com>
2013-04-30 22:41:18 +0900

    fix

--- emacs.d/inits/36-helm-c-moccur.el
+++ emacs.d/inits/36-helm-c-moccur.el
@@ -1,9 +1,5 @@
 (require 'helm-c-moccur)
 (setq
- helm-c-moccur-helm-idle-delay 0.1
- ;; バッファの情報をハイライトする
- helm-c-moccur-higligt-info-line-flag t
- ;; 現在選択中の候補の位置を他のwindowに表示する
- helm-c-moccur-enable-auto-look-flag t)
+ helm-c-moccur-helm-idle-delay 0.1)
 
 (define-key helm-c-moccur-helm-map (kbd "C-h") 'delete-backward-char)

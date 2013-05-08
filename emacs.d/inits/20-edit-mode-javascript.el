87fe42ab5c6d61fff5a89081a5399618b05d5f27
shiba_yu36 <shibayu36@gmail.com>
2013-05-08 17:56:12 +0900

    js2-modeをpackageに

--- emacs.d/inits/20-edit-mode-javascript.el
+++ emacs.d/inits/20-edit-mode-javascript.el
@@ -6,26 +6,13 @@
 
 (defun my-js2-mode-hook ()
   ;; (require 'espresso)
-  (setq ;; espresso-indent-level 4
-        indent-tabs-mode nil
-        c-basic-offset 4)
-  (c-toggle-auto-state 0)
-  (c-toggle-hungry-state 1)
+  ;; (setq ;; espresso-indent-level 4
+  ;;       indent-tabs-mode nil
+  ;;       c-basic-offset 4)
+  ;; (c-toggle-auto-state 0)
+  ;; (c-toggle-hungry-state 1)
   ;; (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
 
-  (slime-js-minor-mode 1)
-
-  (define-key js2-mode-map "\C-\M-\\"
-    '(lambda()
-       (interactive)
-       (insert "/* -----[ ")
-       (save-excursion
-         (insert " ]----- */"))
-       ))
-  (define-key js2-mode-map "\C-m" 'newline-and-indent)
-  (define-key js2-mode-map "\C-\M-q" 'my-indent-sexp)
-  (if (featurep 'js2-highlight-vars)
-      (js2-highlight-vars-mode))
   (message "My JS2 hook"))
 
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)

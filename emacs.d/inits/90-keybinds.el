652604d5e83a70aa11a9bcb995d53241e506b0db
shiba_yu36 <shibayu36@gmail.com>
2013-05-30 22:27:20 +0900

    binding変更

--- emacs.d/inits/90-keybinds.el
+++ emacs.d/inits/90-keybinds.el
@@ -133,3 +133,6 @@
 (global-set-key (kbd "C-c o f") 'helm-open-github-from-file)
 (global-set-key (kbd "C-c o c") 'helm-open-github-from-commit)
 (global-set-key (kbd "C-c o i") 'helm-open-github-from-issues)
+
+;;; server-edit
+(global-set-key (kbd "C-c C-c C-c") 'server-edit)

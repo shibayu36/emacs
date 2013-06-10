ff53fe11dc8b21ccc6889d4a569d64a329f2d5ce
shiba_yu36 <shibayu36@gmail.com>
2013-06-10 12:07:05 +0900

    Revert "flymakeでtempfile作らないように"
    This reverts commit 9bbae71256b1147e41984ff06e52b41542299cc9.

--- emacs.d/inits/40-flymake.el
+++ emacs.d/inits/40-flymake.el
@@ -76,7 +76,7 @@
 
 (defun flymake-perl-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-with-folder-structure))
+                     'flymake-create-temp-inplace))
          (local-file (file-relative-name
                       temp-file
                       (file-name-directory buffer-file-name))))

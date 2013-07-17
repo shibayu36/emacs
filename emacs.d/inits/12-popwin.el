0bc084cf74e7d5653aaaf1f787137761b39cb2fc
shiba_yu36 <shibayu36@gmail.com>
2013-07-17 22:10:28 +0900

    grepバッファのpopwin

--- emacs.d/inits/12-popwin.el
+++ emacs.d/inits/12-popwin.el
@@ -11,4 +11,5 @@
   (push '("*Completions*" :height 0.4) popwin:special-display-config)
   (push '("*compilation*" :height 0.4 :noselect t :stick t) popwin:special-display-config)
   (push '("*Help*" :height 0.5 :noselect t :stick t) popwin:special-display-config)
-  (push '("*quickrun*" :height 0.5 :stick t) popwin:special-display-config))
+  (push '("*quickrun*" :height 0.5 :stick t) popwin:special-display-config)
+  (push '("\*grep\*" :regexp t :height 0.5 :stick t) popwin:special-display-config))

79ae2772dc68b60ff50ed30dd8f992554149037a
shiba_yu36 <shibayu36@gmail.com>
2013-08-05 09:49:41 +0900

    guide-key

---
+++ emacs.d/elisp/elpa/guide-key-20130208.2047/guide-key-pkg.el
@@ -0,0 +1,1 @@
+(define-package "guide-key" "20130208.2047" "Guide the following key bindings automatically and dynamically [github]" (quote ((popwin "0.3.0"))))
---
+++ emacs.d/inits/40-guide-key.el
@@ -0,0 +1,4 @@
+(require 'guide-key)
+(setq guide-key/guide-key-sequence '("C-x r" "C-c o"))
+(setq guide-key/popup-window-position 'bottom)
+(guide-key-mode 1)

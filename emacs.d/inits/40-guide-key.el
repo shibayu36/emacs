7a3ad42b1cac3ba81a710b16964ae0da4774dc05
shiba_yu36 <shibayu36@gmail.com>
2013-08-12 09:22:05 +0900

    guide-key調整

--- emacs.d/inits/40-guide-key.el
+++ emacs.d/inits/40-guide-key.el
@@ -1,4 +1,4 @@
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-c o"))
+(setq guide-key/guide-key-sequence '("C-x r"))
 (setq guide-key/popup-window-position 'bottom)
 (guide-key-mode 1)

832f7c5034e7f6c6a0af8edc0dcd569b59596ec5
shiba_yu36 <shibayu36@gmail.com>
2013-08-12 09:22:22 +0900

    add Vagrantfile to ruby-mode

--- emacs.d/inits/20-edit-mode-ruby.el
+++ emacs.d/inits/20-edit-mode-ruby.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 (add-hook 'ruby-mode-hook
           '(lambda ()

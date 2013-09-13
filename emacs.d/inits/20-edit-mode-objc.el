fd0f1bb2d55736888c0a2326d757a36be8cf5e08
shibayu36 <shibayu36@gmail.com>
2013-09-13 21:48:27 +0900

    objc

--- emacs.d/inits/20-edit-mode-objc.el
+++ emacs.d/inits/20-edit-mode-objc.el
@@ -1,32 +1,38 @@
-;; (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@implementation" . objc-mode))
-;; (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@interface" . objc-mode))
-;; (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@protocol" . objc-mode))
+(add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@implementation" . objc-mode))
+(add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@interface" . objc-mode))
+(add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*\n@protocol" . objc-mode))
 
-;; (add-to-list 'auto-mode-alist '("\\.mm?$" . objc-mode))
-;; (add-to-list 'auto-mode-alist '("\\.h$" . objc-mode))
+(add-to-list 'auto-mode-alist '("\\.mm?$" . objc-mode))
+(add-to-list 'auto-mode-alist '("\\.h$" . objc-mode))
 
-;; (setq ff-other-file-alist
-;;       '(("\\.mm?$" (".h"))
-;;         ("\\.cc$"  (".hh" ".h"))
-;;         ("\\.hh$"  (".cc" ".C"))
+(setq ff-other-file-alist
+      '(("\\.mm?$" (".h"))
+        ("\\.cc$"  (".hh" ".h"))
+        ("\\.hh$"  (".cc" ".C"))
 
-;;         ("\\.c$"   (".h"))
-;;         ("\\.h$"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
+        ("\\.c$"   (".h"))
+        ("\\.h$"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
 
-;;         ("\\.C$"   (".H"  ".hh" ".h"))
-;;         ("\\.H$"   (".C"  ".CC"))
+        ("\\.C$"   (".H"  ".hh" ".h"))
+        ("\\.H$"   (".C"  ".CC"))
 
-;;         ("\\.CC$"  (".HH" ".H"  ".hh" ".h"))
-;;         ("\\.HH$"  (".CC"))
+        ("\\.CC$"  (".HH" ".H"  ".hh" ".h"))
+        ("\\.HH$"  (".CC"))
 
-;;         ("\\.cxx$" (".hh" ".h"))
-;;         ("\\.cpp$" (".hpp" ".hh" ".h"))
+        ("\\.cxx$" (".hh" ".h"))
+        ("\\.cpp$" (".hpp" ".hh" ".h"))
 
-;;         ("\\.hpp$" (".cpp" ".c"))))
+        ("\\.hpp$" (".cpp" ".c"))))
 
-;; (add-hook 'objc-mode-hook
-;;           (lambda ()
-;;             (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (c-set-style "cc-mode")
+             (setq-default tab-width 4)
+             (setq-default indent-tabs-mode nil)))
 
-;;             ;; XCode を利用した補完を有効にする
-;;             (push 'ac-source-company-xcode ac-sources)))
+(add-hook 'objc-mode-hook
+          (lambda ()
+            (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
+
+            ;; XCode を利用した補完を有効にする
+            (push 'ac-source-company-xcode ac-sources)))

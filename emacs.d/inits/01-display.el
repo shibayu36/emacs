3796fe5097cfa0edab78e24b3777f570773dc48c
shibayu36 <shibayu36@gmail.com>
2013-10-12 17:27:26 +0900

    等幅設定がちゃんとなるように

--- emacs.d/inits/01-display.el
+++ emacs.d/inits/01-display.el
@@ -119,14 +119,24 @@
 (font-lock-mode t)
 (font-lock-fontify-buffer)
 
-;;等幅文字設定
-(create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
-(set-fontset-font "fontset-menlokakugo"
-                  'unicode
-                  (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
-                  nil
-                  'append)
-(add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; フォント設定 ;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+(set-face-attribute
+ 'default nil
+ :family "Menlo"
+ :height 120)
+
+(set-fontset-font
+ nil 'japanese-jisx0208
+ (font-spec :family "Hiragino Kaku Gothic ProN"))
+
+(setq face-font-rescale-alist
+      '((".*Menlo.*" . 1.0)
+        ("Hiragino.*" . 1.2)
+        (".*nfmotoyacedar-bold.*" . 1.2)
+        (".*nfmotoyacedar-medium.*" . 1.2)
+        ("-cdac$" . 1.3)))
 
 ;;カラーテーマ
 (require 'color-theme)

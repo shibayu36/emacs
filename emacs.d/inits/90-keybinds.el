b9fd16ce0b320e921d9f89613ab872dd6e05bcd9
shibayu36 <shibayu36@gmail.com>
2013-10-19 19:05:24 +0900

    use helm-occur instead of helm-c-moccur

--- emacs.d/inits/90-keybinds.el
+++ emacs.d/inits/90-keybinds.el
@@ -52,10 +52,9 @@
 (global-set-key (kbd "C-c g") 'anything-git-grep-all)
 (define-key global-map [(control @)] 'anything-for-tags)
 
-;;; helm-c-moccur
-(global-set-key (kbd "C-M-o")
-                'helm-c-moccur-occur-by-moccur)
-(define-key helm-c-moccur-helm-map (kbd "C-c C-a") 'all-from-helm-moccur)
+;;; helm-occur
+(global-set-key (kbd "C-M-o") 'helm-occur)
+(define-key helm-map (kbd "C-c C-a") 'all-from-helm-occur)
 
 
 
@@ -116,7 +115,7 @@
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
 ;;; isearch-mode
-(define-key isearch-mode-map (kbd "C-o") 'helm-c-moccur-from-isearch)
+(define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
 (define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
 
 ;;; direx-mode

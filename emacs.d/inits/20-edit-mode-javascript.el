b8eb2a2166987c9947aa5fc2ce70a950c979b272
shibayu36 <shibayu36@gmail.com>
2013-10-19 19:04:45 +0900

    use javascript mode instead of js2-mode

--- emacs.d/inits/20-edit-mode-javascript.el
+++ emacs.d/inits/20-edit-mode-javascript.el
@@ -1,18 +1,4 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;javascriptモード;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-(defun my-js2-mode-hook ()
-  ;; (require 'espresso)
-  ;; (setq ;; espresso-indent-level 4
-  ;;       indent-tabs-mode nil
-  ;;       c-basic-offset 4)
-  ;; (c-toggle-auto-state 0)
-  ;; (c-toggle-hungry-state 1)
-  ;; (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-
-  (message "My JS2 hook"))
-
-(add-hook 'js2-mode-hook 'my-js2-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))

a473152e82bd3d0e95552535719a25754cfa44d1
shibayu36 <shibayu36@gmail.com>
2013-10-29 21:16:42 +0900

    query-replace keybind

--- emacs.d/inits/90-keybinds.el
+++ emacs.d/inits/90-keybinds.el
@@ -117,6 +117,8 @@
 ;; replace
 (global-set-key (kbd "C-c r") 'anzu-query-replace)
 (global-set-key (kbd "C-c R") 'anzu-query-replace-regexp)
+(global-set-key (kbd "M-%") 'anzu-query-replace)
+(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
 ;; (global-set-key (kbd "C-c r") 'query-replace)
 ;; (global-set-key (kbd "C-c R") 'foreign-regexp/query-replace)
 

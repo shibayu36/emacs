f9717e9e9f776397239283a5117216788a0849cf
shibayu36 <shibayu36@gmail.com>
2013-11-10 14:58:21 +0900

    flyspell mode

---
+++ emacs.d/inits/20-edit-mode-flyspell.el
@@ -0,0 +1,12 @@
+(require 'flyspell)
+(require 'ispell)
+(setq ispell-program-name "aspell")
+
+;;; 日本語混じりの文章チェック
+(eval-after-load "ispell"
+ '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+
+;;; keybinding取られるの防ぐ
+(define-key flyspell-mode-map (kbd "C-;") nil)
+(define-key flyspell-mode-map (kbd "C-.") nil)
+(define-key flyspell-mode-map (kbd "C-,") nil)

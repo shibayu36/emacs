2022231c66112066fb13dc50471a43f52adf1941
shibayu36 <shibayu36@gmail.com>
2013-11-27 09:16:27 +0900

    popwin interval

--- emacs.d/inits/12-popwin.el
+++ emacs.d/inits/12-popwin.el
@@ -1,5 +1,7 @@
 (setq pop-up-windows nil)
 (require 'popwin nil t)
+(setq popwin:close-popup-window-timer-interval 0.5)
+
 (when (require 'popwin nil t)
   (setq anything-samewindow nil)
   (setq display-buffer-function 'popwin:display-buffer)

85e1bb0235fff7192fc95ec968befe09094dcee8
shibayu36 <shibayu36@gmail.com>
2013-12-29 20:43:02 +0900

    diredの文字コード一括変更いらない

--- emacs.d/inits/10-dired.el
+++ emacs.d/inits/10-dired.el
@@ -31,45 +31,6 @@
     (dired-mark arg)
     (dired-previous-line 1)))
 
-;;文字コードの一括変換用設定
-;;; dired を使って、一気にファイルの coding system (漢字) を変換する
-;; (require 'dired-aux)
-
-;; (defvar dired-default-file-coding-system nil
-;;   "*Default coding system for converting file (s).")
-
-;; (defvar dired-file-coding-system 'no-conversion)
-
-;; (defun dired-convert-coding-system ()
-;;   (let ((file (dired-get-filename))
-;;         (coding-system-for-write dired-file-coding-system)
-;;         failure)
-;;     (condition-case err
-;;         (with-temp-buffer
-;;           (insert-file file)
-;;           (write-region (point-min) (point-max) file))
-;;       (error (setq failure err)))
-;;     (if (not failure)
-;;         nil
-;;       (dired-log "convert coding system error for %s:\n%s\n" file failure)
-;;       (dired-make-relative file))))
-
-;; (defun dired-do-convert-coding-system (coding-system &optional arg)
-;;   "Convert file (s) in specified coding system."
-;;   (interactive
-;;    (list (let ((default (or dired-default-file-coding-system
-;;                             buffer-file-coding-system)))
-;;            (read-coding-system
-;;             (format "Coding system for converting file (s) (default, %s): "
-;;                     default)
-;;             default))
-;;          current-prefix-arg))
-;;   (check-coding-system coding-system)
-;;   (setq dired-file-coding-system coding-system)
-;;   (dired-map-over-marks-check
-;;    (function dired-convert-coding-system) arg 'convert-coding-system t))
-
-
 ;;diredで新しいバッファを作成しない
 (defun dired-my-advertised-find-file ()
   (interactive)

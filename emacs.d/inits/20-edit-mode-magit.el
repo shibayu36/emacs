571c129b1ab854fcaf861cd5fedc08f4154de992
shibayu36 <shibayu36@gmail.com>
2014-01-29 20:36:35 +0900

    magit実行後にwindow構成が変わらないように

---
+++ emacs.d/inits/20-edit-mode-magit.el
@@ -0,0 +1,16 @@
+(require 'magit)
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun my/magit-quit-session ()
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+
+(defadvice git-commit-commit (after move-to-magit-buffer activate)
+  (delete-window))

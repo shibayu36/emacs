2c93ae5fa4ef38b542e017c4e60a284fdddcc8d9
shibayu36 <shibayu36@gmail.com>
2014-01-30 17:36:35 +0900

    yasnippet

--- emacs.d/inits/40-yasnippet.el
+++ emacs.d/inits/40-yasnippet.el
@@ -1,36 +1,36 @@
 ;;初期設定
 (require 'yasnippet)
 
-;; (setq yas-snippet-dirs
-;;       '("~/.emacs.d/snippets" "~/Dropbox/config-file/.emacs.d/snippets"))
-;; (yas-global-mode 1)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets" "~/Dropbox/config-file/.emacs.d/snippets"))
+(yas-global-mode 1)
 
-;; (define-key yas-minor-mode-map [(tab)] nil)
-;; (define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map [(tab)] nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
 
-;; (setq yas-trigger-key "C-M-y")
+(setq yas-trigger-key "C-M-y")
 
 ;; anything interface
-;; (eval-after-load "anything-config"
-;;   '(progn
-;;      (defun my-yas/prompt (prompt choices &optional display-fn)
-;;        (let* ((names (loop for choice in choices
-;;                            collect (or (and display-fn (funcall display-fn choice))
-;;                                        choice)))
-;;               (selected (anything-other-buffer
-;;                          `(((name . ,(format "%s" prompt))
-;;                             (candidates . names)
-;;                             (action . (("Insert snippet" . (lambda (arg) arg))))))
-;;                          "*anything yas/prompt*")))
-;;          (if selected
-;;              (let ((n (position selected names :test 'equal)))
-;;                (nth n choices))
-;;            (signal 'quit "user quit!"))))
-;;      (setq yas/prompt-functions '(my-yas/prompt))))
+(eval-after-load "anything-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (anything-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*anything yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (setq yas/prompt-functions '(my-yas/prompt))))
 
-;; (defun yas/perl-package-name ()
-;;   (let ((file-path (file-name-sans-extension (buffer-file-name))))
-;;     (if (string-match "lib/" file-path)
-;;         (replace-regexp-in-string "/" "::"
-;;                                   (car (last (split-string file-path "/lib/"))))
-;;       (file-name-nondirectory file-path))))
+(defun yas/perl-package-name ()
+  (let ((file-path (file-name-sans-extension (buffer-file-name))))
+    (if (string-match "lib/" file-path)
+        (replace-regexp-in-string "/" "::"
+                                  (car (last (split-string file-path "/lib/"))))
+      (file-name-nondirectory file-path))))

94b43097bd0fd0b71790f6cf262b5d7ae2ea91da
shibayu36 <shibayu36@gmail.com>
2014-02-01 18:27:33 +0900

    いくつかmode-lineから消した

---
+++ emacs.d/inits/01-mode-line.el
@@ -0,0 +1,3 @@
+(setq anzu-mode-lighter nil)
+(setq undo-tree-mode-lighter nil)
+(setq git-gutter:lighter nil)
--- emacs.d/inits/20-edit-mode-perl.el
+++ emacs.d/inits/20-edit-mode-perl.el
@@ -41,10 +41,10 @@
             (setq plcmp-use-keymap nil)
             (require 'perl-completion)
             (perl-completion-mode t)
+            (setq plcmp-default-lighter nil)
             (add-to-list 'ac-sources 'ac-source-my-perl-completion)
             (local-set-key (kbd "M-RET") 'plcmp-cmd-smart-complete)
-            (local-set-key (kbd "C-c d") 'plcmp-cmd-show-doc-at-point)
-            ))
+            (local-set-key (kbd "C-c d") 'plcmp-cmd-show-doc-at-point)))
 
 (add-hook 'cperl-mode-hook
           '(lambda ()

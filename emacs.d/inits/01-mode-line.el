582b07dbfa0afd273d6c7f3e4024ed87a8361e6a
shibayu36 <shibayu36@gmail.com>
2014-02-02 14:25:41 +0900

    mode系のshortenを設定

--- emacs.d/inits/01-mode-line.el
+++ emacs.d/inits/01-mode-line.el
@@ -1,3 +1,33 @@
-(setq anzu-mode-lighter nil)
-(setq undo-tree-mode-lighter nil)
-(setq git-gutter:lighter nil)
+(defvar mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (flymake-mode . " Fm")
+    (paredit-mode . "")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (undo-tree-mode . "")
+    (git-gutter-mode . "")
+    (anzu-mode . "")
+    (yas-minor-mode . "")
+    (guide-key-mode . "")
+
+    ;; Major modes
+    (dired-mode . "Dir")
+    (lisp-interaction-mode . "Li")
+    (cperl-mode . "Pl")
+    (python-mode . "Py")
+    (ruby-mode   . "Rb")
+    (emacs-lisp-mode . "El")
+    (markdown-mode . "Md")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)

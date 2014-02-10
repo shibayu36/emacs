fc25af8dc5ed604fa438ef707915e26fa172d7ce
shibayu36 <shibayu36@gmail.com>
2014-02-10 17:47:48 +0900

    Utility for sending command to iTerm

--- emacs.d/inits/00-util.el
+++ emacs.d/inits/00-util.el
@@ -21,3 +21,15 @@
           (shell-command-to-string "git rev-parse --show-cdup")))
         (t
          "")))
+
+;;; iterm
+(defun execute-on-iterm (command)
+  (interactive "MCommand: ")
+  (do-applescript
+   (format "tell application \"iTerm\"
+              activate
+              tell current session of current terminal
+                write text \"%s\"
+              end tell
+            end tell"
+           command)))

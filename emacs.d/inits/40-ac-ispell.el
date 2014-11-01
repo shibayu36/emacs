9408b6b98c105c649b43fe36da5be0e3b9ec1ddb
shibayu36 <shibayu36@gmail.com>
2014-11-01 14:02:08 +0900

    remove useless

--- emacs.d/inits/40-ac-ispell.el
+++ emacs.d/inits/40-ac-ispell.el
@@ -3,6 +3,6 @@
   '(progn
       (ac-ispell-setup)))
 
-(add-hook 'git-commit-mode-hook 'ac-ispell-ac-setup)
+;; (add-hook 'git-commit-mode-hook 'ac-ispell-ac-setup)
 ;; (add-hook 'text-mode-hook 'ac-ispell-ac-setup)
 

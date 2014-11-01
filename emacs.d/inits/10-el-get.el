74eab7d030cea09c2276e30d8c3f1a4e89ada732
shibayu36 <shibayu36@gmail.com>
2014-11-01 14:01:34 +0900

    Revert

---
+++ emacs.d/inits/10-el-get.el
@@ -0,0 +1,18 @@
+(setq el-get-dir "~/.emacs.d/elisp/el-get/")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; Packages to install from el-get
+(defvar my/el-get-packages
+  '(
+    smartchr
+    python-mode
+    )
+  "A list of packages to install from el-get at launch.")
+
+(el-get 'sync my/el-get-packages)

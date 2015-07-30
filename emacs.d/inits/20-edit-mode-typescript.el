32550fef4f4f78b1001536255468af1879ee3672
shibayu36 <shibayu36@gmail.com>
2015-07-30 16:16:47 +0900

    require

--- emacs.d/inits/20-edit-mode-typescript.el
+++ emacs.d/inits/20-edit-mode-typescript.el
@@ -1,6 +1,7 @@
 (require 'typescript-mode)
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
+(require 'tide)
 (add-hook 'typescript-mode-hook
           (lambda ()
             (tide-setup)

2b49af9a807af4331b41943540ca0430106130b6
shibayu36 <shibayu36@gmail.com>
2015-08-04 15:47:20 +0900

    markdown-modeでの変なインデントを調整

--- emacs.d/inits/20-edit-mode-markdown.el
+++ emacs.d/inits/20-edit-mode-markdown.el
@@ -1,3 +1,6 @@
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-hook 'markdown-mode-hook
+          '(lambda ()
+             (electric-indent-local-mode -1)))

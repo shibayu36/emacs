1ce5aa852ee86663ec322fdc4c6b9656d2a3141d
shibayu36 <shibayu36@gmail.com>
2015-11-13 09:15:52 +0900

    typescript and tide keybinds

--- emacs.d/inits/20-edit-mode-typescript.el
+++ emacs.d/inits/20-edit-mode-typescript.el
@@ -1,7 +1,15 @@
 (require 'typescript-mode)
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
+(with-eval-after-load 'typescript-mode
+  (dolist (key '("{" "}" "(" ")" ":" ";" ","))
+    (define-key typescript-mode-map key nil)))
+
 (require 'tide)
+(with-eval-after-load 'tide
+  (define-key typescript-mode-map (kbd "C-@") 'tide-jump-to-definition)
+  (define-key typescript-mode-map (kbd "M-@") 'tide-jump-back))
+
 (add-hook 'typescript-mode-hook
           (lambda ()
             (tide-setup)

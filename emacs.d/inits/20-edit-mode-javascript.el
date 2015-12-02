df13050087a4be0225a882ef6ebf3046d6e58469
shibayu36 <shibayu36@gmail.com>
2015-12-02 11:41:52 +0900

    js2-mode

--- emacs.d/inits/20-edit-mode-javascript.el
+++ emacs.d/inits/20-edit-mode-javascript.el
@@ -1,6 +1,4 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;javascriptモード;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'auto-mode-alist '("\\.js$" . javascript-mode))
-
-(add-hook 'js-mode-hook (lambda () (flycheck-mode t)))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))

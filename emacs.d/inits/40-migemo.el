1a8911372d5f9ae280d37dabde51b245f481260a
shibayu36 <shibayu36@gmail.com>
2015-12-14 19:08:38 +0900

    migemo

--- emacs.d/inits/40-migemo.el
+++ emacs.d/inits/40-migemo.el
@@ -1,19 +1,38 @@
 (when (and (executable-find "cmigemo")
            (require 'migemo nil t))
-  ;; cmigemoを使う
+
   (setq migemo-command "cmigemo")
-  ;; migemoのコマンドラインオプション
-  (setq migemo-options '("-q" "--emacs" "-i" "\a"))
-  ;; migemo辞書の場所
+  (setq migemo-options '("-q" "--emacs"))
   (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-  ;; cmigemoで必須の設定
+  (setq migemo-coding-system 'utf-8)
+
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
+
   ;; キャッシュの設定
   (setq migemo-use-pattern-alist t)
   (setq migemo-use-frequent-pattern-alist t)
   (setq migemo-pattern-alist-length 1000)
-  (setq migemo-coding-system 'utf-8-unix)
 
-  ;; migemoを起動する
-  (migemo-init))
+  (load-library "migemo")
+  (migemo-init)
+
+  ;; 昔の設定、とりあえず残しておく
+  ;; ;; cmigemoを使う
+  ;; (setq migemo-command "cmigemo")
+  ;; ;; migemoのコマンドラインオプション
+  ;; (setq migemo-options '("-q" "--emacs" "-i" "\a"))
+  ;; ;; migemo辞書の場所
+  ;; (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  ;; ;; cmigemoで必須の設定
+  ;; (setq migemo-user-dictionary nil)
+  ;; (setq migemo-regex-dictionary nil)
+  ;; ;; キャッシュの設定
+  ;; (setq migemo-use-pattern-alist t)
+  ;; (setq migemo-use-frequent-pattern-alist t)
+  ;; (setq migemo-pattern-alist-length 1000)
+  ;; (setq migemo-coding-system 'utf-8-unix)
+
+  ;; ;; migemoを起動する
+  ;; (migemo-init)
+  )

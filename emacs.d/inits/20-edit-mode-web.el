71a282bc754969c6a99ca4af900f0d1c7d829771
shibayu36 <shibayu36@gmail.com>
2016-01-05 22:18:47 +0900

    web-mode indent

--- emacs.d/inits/20-edit-mode-web.el
+++ emacs.d/inits/20-edit-mode-web.el
@@ -4,10 +4,16 @@
 (setq web-mode-engines-alist
       '(("template-toolkit" . "\\.html?\\'" )))
 
+(defun web-mode-element-close-and-indent ()
+  (interactive)
+  (web-mode-element-close)
+  (indent-for-tab-command))
+
 (add-hook 'web-mode-hook
           '(lambda ()
              (require 'hatena-translator)
              (setq web-mode-markup-indent-offset 2)
+             (define-key web-mode-map (kbd "C-c /") 'web-mode-element-close-and-indent)
              (define-key web-mode-map [(meta t)] 'hatena-translator:popup-msgid-at-point)
              (define-key web-mode-map [(meta T)] 'hatena-translator:open-msgid-at-point)
              (define-key web-mode-map (kbd "T") (smartchr '("T" "[%- `!!' %]" "[% `!!' %]")))))

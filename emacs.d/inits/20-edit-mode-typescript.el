eb7bc0e75d8e11639dae74c265feac53c5d05bf1
shibayu36 <shibayu36@gmail.com>
2016-03-27 18:56:40 +0900

    js test

--- emacs.d/inits/20-edit-mode-typescript.el
+++ emacs.d/inits/20-edit-mode-typescript.el
@@ -10,10 +10,28 @@
   (define-key typescript-mode-map (kbd "C-@") 'tide-jump-to-definition)
   (define-key typescript-mode-map (kbd "M-@") 'tide-jump-back))
 
+(defun run-js-mocha-describe-test ()
+  (interactive)
+  (let* ((topdir (git-root-directory))
+         (test-grep-args nil))
+    (save-excursion
+      (when (or
+             (re-search-backward "\\bdescribe(\s*'\\(.*?\\)'" nil t)
+             (re-search-forward "\\bdescribe(\s*'\\(.*?\\)'" nil t))
+        (setq test-grep-args (match-string 1))))
+    (if test-grep-args
+        (quickrun
+         :source
+         `((:command . "$(npm bin)/karma")
+           (:default-directory . ,topdir)
+           (:exec . (,(concat "%c run --no-refresh -- --grep " test-grep-args))))))))
+
+
 (add-hook 'typescript-mode-hook
           (lambda ()
             (tide-setup)
             (flycheck-mode t)
             (setq flycheck-check-syntax-automatically '(save mode-enabled))
             (eldoc-mode t)
-            (company-mode-on)))
+            (company-mode-on)
+            (local-set-key (kbd "C-c t") 'run-js-mocha-describe-test)))

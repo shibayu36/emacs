ad0faddd549515980b7e1d3fa57660109f1df81f
shibayu36 <shibayu36@gmail.com>
2017-04-02 14:09:54 +0900

    js2-modeのoffsetはひとまず2に

--- emacs.d/inits/20-edit-mode-javascript.el
+++ emacs.d/inits/20-edit-mode-javascript.el
@@ -2,6 +2,7 @@
 ;;;;;;;javascriptモード;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq js2-basic-offset 2)
 
 ;;; js2-modeでのalignルール
 (add-hook

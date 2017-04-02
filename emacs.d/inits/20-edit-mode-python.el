de1911fd51bbf3f14cdb11aef9cab34021cd37f9
shibayu36 <shibayu36@gmail.com>
2017-04-02 15:06:30 +0900

    methodのtooltipドキュメントはminibufferに表示

--- emacs.d/inits/20-edit-mode-python.el
+++ emacs.d/inits/20-edit-mode-python.el
@@ -5,6 +5,11 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
+;; methodのドキュメントはminibufferに表示
+(setq jedi:tooltip-method 'nil)
+(set-face-attribute 'jedi:highlight-function-argument nil
+                    :foreground "green")
+
 (require 'virtualenvwrapper)
 (require 'auto-virtualenvwrapper)
 (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)

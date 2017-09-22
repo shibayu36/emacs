9fd39f0297a31cf024872753953be1f2a2faf1ae
shibayu36 <shibayu36@gmail.com>
2017-09-22 11:30:38 +0900

    razor

--- emacs.d/inits/20-edit-mode-web.el
+++ emacs.d/inits/20-edit-mode-web.el
@@ -5,18 +5,20 @@
 (setq web-mode-engines-alist
       '(
         ("template-toolkit" . "\\.html?\\'" )
-        ("razor" . "scala\\.html\\'")))
+;;        ("razor" . "scala\\.html\\'")
+        ))
 
 (defun web-mode-element-close-and-indent ()
   (interactive)
   (web-mode-element-close)
   (indent-for-tab-command))
 
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-enable-auto-indentation nil)
 (add-hook 'web-mode-hook
           '(lambda ()
              (require 'hatena-translator)
-             (setq web-mode-markup-indent-offset 2)
-             (setq web-mode-enable-auto-indentation nil)
              (define-key web-mode-map (kbd "C-c /") 'web-mode-element-close-and-indent)
              (define-key web-mode-map [(meta t)] 'hatena-translator:popup-msgid-at-point)
              (define-key web-mode-map [(meta T)] 'hatena-translator:open-msgid-at-point)

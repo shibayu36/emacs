63d1bb31e37499c21fe02d7334d40a249b995a8e
shibayu36 <shibayu36@gmail.com>
2018-12-18 22:44:40 +0900

    lspの設定

---
+++ emacs.d/inits/13-lsp.el
@@ -0,0 +1,9 @@
+(require 'lsp-mode)
+(require 'lsp-clients)
+(require 'company-lsp)
+(setq lsp-auto-guess-root t)
+(setq lsp-auto-configure nil)
+
+;; (require 'lsp-ui)
+;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; (setq lsp-ui-sideline-show-symbol nil)

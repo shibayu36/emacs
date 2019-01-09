0f9484aeed2a869fb51e699a52cd5accd2d76e5d
shibayu36 <shibayu36@gmail.com>
2019-01-09 17:25:09 +0900

    fontを設定して外部ディスプレイでもにじまないように

--- emacs.d/inits/01-display.el
+++ emacs.d/inits/01-display.el
@@ -67,21 +67,32 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; フォント設定 ;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
-(set-face-attribute
- 'default nil
- :family "Menlo"
- :height 120)
 
-(set-fontset-font
- nil 'japanese-jisx0208
- (font-spec :family "Hiragino Kaku Gothic ProN"))
-
-(setq face-font-rescale-alist
-      '((".*Menlo.*" . 1.0)
-        ("Hiragino.*" . 1.2)
-        (".*nfmotoyacedar-bold.*" . 1.2)
-        (".*nfmotoyacedar-medium.*" . 1.2)
-        ("-cdac$" . 1.3)))
+;; http://d.hatena.ne.jp/setoryohei/20110117/1295336454
+;; フレームのフォントを設定
+(let* ((size 12) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+       (asciifont "Menlo") ; ASCIIフォント
+       (jpfont "Hiragino Kaku Gothic ProN") ; 日本語フォント
+       (applefont "AppleMyungjo")
+       (h (* size 10))
+       (fontspec (font-spec :family asciifont))
+       (jp-fontspec (font-spec :family jpfont))
+       (apple-fontspec (font-spec :family applefont)))
+  (set-face-attribute 'default nil :family asciifont :height h)
+  (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font nil '(#x0370 . #x03FF) fontspec)    ; ギリシャ文字
+  (set-fontset-font nil '(#xE000 . #xF8FF) apple-fontspec)    ; アップルマークとか
+  )
+;; フォントサイズの比を設定
+(dolist (elt '(("^-apple-hiragino.*" . 1.2)
+               (".*osaka-bold.*" . 1.2)
+               (".*osaka-medium.*" . 1.2)
+               (".*courier-bold-.*-mac-roman" . 1.0)
+               (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+               (".*monaco-bold-.*-mac-roman" . 0.9)))
+  (add-to-list 'face-font-rescale-alist elt))
 
 (defun font-big ()
   (interactive)

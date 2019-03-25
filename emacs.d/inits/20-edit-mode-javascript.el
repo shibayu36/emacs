28c7827522917277ef3887e01fbf9b0ba4f0347b
shibayu36 <shibayu36@gmail.com>
2019-03-25 09:48:42 +0900

    hook

--- emacs.d/inits/20-edit-mode-javascript.el
+++ emacs.d/inits/20-edit-mode-javascript.el
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook
- 'js2-mode
+ 'js2-mode-hook
  (lambda ()
    (setq js2-basic-offset 2)))
 (setq js2-basic-offset 2)

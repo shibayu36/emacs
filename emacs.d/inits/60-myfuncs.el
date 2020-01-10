049aefbc42fd88978d906af0a57a551820f0613e
shibayu36 <shibayu36@gmail.com>
2020-01-10 14:52:46 +0900

    3分割にする画面幅もっと小さくていい

--- emacs.d/inits/60-myfuncs.el
+++ emacs.d/inits/60-myfuncs.el
@@ -20,7 +20,7 @@
 (defun other-window-or-split ()
   (interactive)
   (when (one-window-p)
-    (if (>= (window-body-width) 270)
+    (if (>= (window-body-width) 210)
         (split-window-horizontally-n 3)
       (split-window-horizontally)))
   (other-window 1))

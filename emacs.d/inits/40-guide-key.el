8cd422c220fea9b65dee11ecb7168d6f90044525
shibayu36 <shibayu36@gmail.com>
2020-01-10 14:52:39 +0900

    guide key使ってない

--- emacs.d/inits/40-guide-key.el
+++ emacs.d/inits/40-guide-key.el
@@ -1,4 +1,4 @@
-(require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r"))
-(setq guide-key/popup-window-position 'bottom)
-(guide-key-mode 1)
+;; (require 'guide-key)
+;; (setq guide-key/guide-key-sequence '("C-x r"))
+;; (setq guide-key/popup-window-position 'bottom)
+;; (guide-key-mode 1)

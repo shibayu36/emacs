5c610db63e012830a1efb7030f9af3b9c7cc1881
shibayu36 <shibayu36@gmail.com>
2020-01-10 14:52:32 +0900

    anzu止める

--- emacs.d/inits/40-anzu.el
+++ emacs.d/inits/40-anzu.el
@@ -1,8 +1,8 @@
-(require 'anzu)
+;; (require 'anzu)
 
-(global-anzu-mode +1)
-(setq anzu-use-migemo t)
-(setq anzu-search-threshold 1000)
-(setq anzu-minimum-input-length 3)
+;; (global-anzu-mode +1)
+;; (setq anzu-use-migemo t)
+;; (setq anzu-search-threshold 1000)
+;; (setq anzu-minimum-input-length 3)
 
-(set-face-attribute 'anzu-mode-line nil :foreground "#fff" :weight 'bold)
+;; (set-face-attribute 'anzu-mode-line nil :foreground "#fff" :weight 'bold)
